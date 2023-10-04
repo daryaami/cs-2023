4f57f69fcc6c8592dde795bc22c91b58c77cbb02
daryaami <daryaami10@gmail.com>
2023-10-04 15:52:49 +0300

    fixed lab-3

--- lab-3/script.bash
+++ lab-3/script.bash
@@ -1,32 +1,44 @@
 #!/bin/bash
 export LANG=en_US.UTF-8
 
+dirs_count=0
+files_count=0
+
+vertical=$'\u2502\u00A0\u00A0\u0020'
+last_line=$'\u2514\u2500\u2500\u0020'
+middle_line=$'\u251c\u2500\u2500\u0020'
+parent=$'\u2502\u00A0\u00A0\u0020'
+space=$'\u00A0\u00A0\u00A0\u0020'
+
 build_tree()
 {
-	local dir=$1
-	local indent=$2
-	
-	children=("$dir"/*)
-	child_count=${#children[@]}
-	
-	
-	for item in ${children[@]}; do
-		echo -n "$indent"
-		printf "\u251c\u2500\u2500\u0020"
-		echo "$(basename $item)"
-		if [ -d "$item" ]; then
-			build_tree "$item" $'\u2502\u00A0\u00A0\u0020'$indent
-			((count_dirs++))
-		elif [ -f "$item" ]; then
-			((count_files++))
+	cd $1
+	local subdirs=($(ls)) #создаем массив с вложенными директориями
+
+	for idx in ${!subdirs[@]}; do #проход циклом по индексам массива
+		item=${subdirs[$idx]}
+		if [[ $idx -eq $((${#subdirs[@]}-1)) ]]; then
+			echo "$2$last_line$item"
+		else
+			echo "$2$middle_line$item"
+		fi
+		
+		if [ -d $item ]; then
+			((dirs_count++))
+			if [[ $idx -eq $((${#subdirs[@]}-1)) ]]; then
+				build_tree $item "$2$space"
+			else
+				build_tree $item "$2$vertical"
+			fi
+		else
+			((files_count++))
 		fi
 	done
+	
+	cd ..
 }
 
-path=$1
-echo "$path"
-count_dirs=0
-count_files=0
+echo $1
 build_tree $1 ""
 echo
-echo "$count_dirs diretories, $count_files files"
+echo "$dirs_count diretories, $files_count files"
